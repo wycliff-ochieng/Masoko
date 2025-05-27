4d23eae472ca7d0e2b33595377a65fe92de50d05
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-05-27 17:28:17 +0300

    login/register handlers

---
+++ handlers/users.go
@@ -0,0 +1,20 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+)
+
+type User struct {
+	l *log.Logger
+}
+
+func NewUser(l *log.Logger) *User {
+	return &User{l}
+}
+
+func (u *User) UserLogin(w http.ResponseWriter, r *http.Request) {
+	return
+}
+
+func (u *User) RegisterUser(w http.ResponseWriter, r *http.Request)
