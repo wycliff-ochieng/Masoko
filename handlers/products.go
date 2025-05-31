9c1a35f1b134c644ef013ddcd1e19ca0de474782
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-06-01 01:36:25 +0300

    implemented createproduct handler

--- handlers/products.go
+++ handlers/products.go
@@ -6,10 +6,39 @@ import (
 	"net/http"
 
 	"github.com/wycliff-ochieng/cmd/migrate"
+	"github.com/wycliff-ochieng/db"
 )
 
 type Product struct {
-	l *log.Logger
+	l     *log.Logger
+	store db.Storage
+}
+
+func NewProduct(l *log.Logger, store db.Storage) *Product {
+	return &Product{
+		l:     l,
+		store: store,
+	}
+}
+
+type APIFunc func(w http.ResponseWriter, r *http.Request) error
+
+type APIError struct {
+	Error string
+}
+
+func MakeHttpHandlerFunc(a APIFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := a(w, r); err != nil {
+			WriteJSON(w, http.StatusRequestTimeout, APIError{Error: err.Error()})
+		}
+	}
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Centent-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
 }
 
 func (p *Product) GetProducts(w http.ResponseWriter, r *http.Request) {
@@ -25,7 +54,24 @@ func (p *Product) GetProducts(w http.ResponseWriter, r *http.Request) {
 	w.Write(d)
 }
 
-func (p *Product) CreateProduct(w http.ResponseWriter, r *http.Request) {
+func (p *Product) CreateProduct(w http.ResponseWriter, r *http.Request) error {
+	p.l.Println("Handle GetPRoduct from db")
 
-	p.l.Println("Handle POST products")
+	createproductreq := new(migrate.CreateProductReq)
+
+	if err := json.NewDecoder(r.Body).Decode(createproductreq); err != nil {
+		return err
+	}
+
+	product := migrate.NewProduct(createproductreq.Name, createproductreq.Price, createproductreq.Description, createproductreq.Stock)
+
+	if err := p.store.CreateProduct(product); err != nil {
+		return err
+	}
+	return WriteJSON(w, http.StatusOK, product)
 }
+
+//func (p *Product) CreateProduct(w http.ResponseWriter, r *http.Request) {
+//
+//	p.l.Println("Handle POST products")
+//}
