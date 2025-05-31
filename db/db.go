d4a4a43a48d5bfba5aa37eadd92bcf786bc06812
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-06-01 01:34:32 +0300

    update database schema && table creation

--- db/db.go
+++ db/db.go
@@ -3,6 +3,8 @@ package db
 import (
 	"database/sql"
 
+	"github.com/wycliff-ochieng/cmd/migrate"
+
 	_ "github.com/lib/pq"
 )
 
@@ -26,12 +28,33 @@ func NewPostgrestore() (*Postgrestore, error) {
 }
 
 func (p *Postgrestore) Init() error {
-	return p.CreateUserTable()
+	return p.CreateProductTable()
 }
 
 func (p *Postgrestore) CreateUserTable() error {
 	query := "CREATE TABLE IF NOT EXISTS users()"
 	_, err := p.db.Exec(query)
+	return err
+}
+
+func (p *Postgrestore) CreateProductTable() error {
+	query := `CREATE TABLE IF NOT EXISTS products(
+	id serial primary key,
+	name varchar(15) not null,
+	price decimal(10,2) not null,
+	description text,
+	stock int default 0,
+	createdat timestamp,
+	updatedat timestamp
+	);`
+
+	_, err := p.db.Exec(query)
+	return err
+}
+
+func (s *Postgrestore) CreateProduct(product *migrate.Product) error {
+	query := `INSERT INTO products (name, description, price) VALUES ($1, $2, $3) RETURNING id`
+	err := s.db.QueryRow(query, product.Name, product.Description, product.Price).Scan(&product.ID)
 	return err
 }
 
