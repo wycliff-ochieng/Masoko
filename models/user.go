7d13ca1d03b15a94432a1101ad07d952e76c9e76
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-06-11 18:51:28 +0300

    user response and user model

--- models/user.go
+++ models/user.go
@@ -4,18 +4,33 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
 	ID        uuid.UUID
-	Email     string
-	Password  string
 	FirstName string
 	LastName  string
+	Email     string
+	Password string
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
 
-func NewUser(email, password, firstname, lastname string) (*User, error) {
-	return &User{}
+func NewUser(firstname, lastname, email, password string) (*User,error){
+	harshedPassword,err := bcrypt.GenerateFromPassword([]byte(password),bcrypt.DefaultCost)
+	if err != nil{
+		return nil,err
+	}
+	
+	now := time.Now()
+	return &User{
+		ID:uuid.New(),
+		FirstName: firstname,
+		LastName: lastname,
+		Email: email,
+		Password: string(harshedPassword),
+		CreatedAt: now,
+		UpdatedAt: now,
+	},nil
 }
