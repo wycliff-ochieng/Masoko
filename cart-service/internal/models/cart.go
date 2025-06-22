6fa767f056631a26d3c73546a536f862d8440895
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-06-22 16:24:06 +0300

    refactoring cart itemm model

--- cart-service/internal/models/cart.go
+++ cart-service/internal/models/cart.go
@@ -1,25 +1,36 @@
 package models
 
-import "time"
+import (
+	"time"
 
+	"github.com/google/uuid"
+)
 
-
-
-type Cart struct{
-	ID string
-	UserID string
+type Cart struct {
+	ID        string
+	UserID    uuid.UUID
 	SessionId string
-	Status string
-	Items []Item
-	Total float64
+	Status    string
+	Items     []CartItem
+	Total     float64
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
 
-
-type Item struct{
+type CartItem struct {
+	CartID    string
 	ProductID string
-	Quantity int
-	Price float64
-	Subtotal float64
+	Quantity  int
+	Price     float64
+	Subtotal  float64
+}
+
+func (c *Cart) CalculateCartTotal() {
+
+	var total float64
+
+	for _, item := range c.Items {
+		total += item.Price * float64(item.Quantity)
+	}
+	c.Total = total
 }
