5e864ed3485a4192c0fe6754d311586572c37b49
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-06-22 16:20:00 +0300

    adding cart and cart item table creation

--- cart-service/internal/database/cartdb.go
+++ cart-service/internal/database/cartdb.go
@@ -3,47 +3,89 @@ package database
 import (
 	"database/sql"
 	"fmt"
+
+	_ "github.com/lib/pq"
 )
+
+type DBInterface interface {
+	QueryRow(query string, args ...interface{}) *sql.Row
+	Exec(query string, args ...interface{}) (sql.Result, error)
+	Close() error
+}
 
 type Postgres struct {
 	db *sql.DB
 }
 
-func NewPostgrestore() (*Postgres, error) {
-	connStr := "user=postgres dbname=cart_service password=gobank sslmode=disable"
+func NewPostgres() (*Postgres, error) {
+	connStr := "user=postgres dbname=postgres password=gobank sslmode=disable"
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		return nil,fmt.Errorf("could not open db %v", err)
+		return nil, fmt.Errorf("could not open db %v", err)
 	}
 
 	if err = db.Ping(); err != nil {
-		return nil,fmt.Errorf("Failed to ping %v", err)
+		return nil, fmt.Errorf("failed to ping %v", err)
 	}
 	return &Postgres{db: db}, nil
 }
 
-
-func(p *Postgres) Init() error{
+func (p *Postgres) Init() error {
 	return p.CreateCartTable()
 }
 
+func (p *Postgres) Init2() error {
+	return p.CreateCartItemTable()
+}
 
-func(p *Postgres) CreateCartTable() error{
-	query := `CREATE TABLE cart(
+func (p *Postgres) CreateCartTable() error {
+	query := `CREATE TABLE carts(
 	ID SERIAL PRIMARY KEY,
-	UserID INT
+	UserID INT,
 	SessionID TEXT,
 	Status VARCHAR(20),
 	Items TEXT,
 	CreatedAT TIMESTAMP,
-	UpdatedAT TIMESTAMP
-	CONSTRAINT fk_userID FOREIGN KEY (UserID)
-	REFERENCES users)`
+	UpdatedAT TIMESTAMP,
+	CONSTRAINT fk_userID FOREIGN KEY (UserID) REFERENCES users)`
 
-	_,err := p.db.Exec(query)
-	if err != nil{
-		return fmt.Errorf("failed to create table: %v",err)
+	_, err := p.db.Exec(query)
+	if err != nil {
+		return fmt.Errorf("failed to create table: %v", err)
 	}
 	return nil
+}
+
+func (p *Postgres) CreateCartItemTable() error {
+	query := `CREATE TABLE cart_items(
+	id SERIAL PRIMARY KEY,
+	cartID INTEGER, 
+	productID INTEGER,
+	quantity INTEGER,
+	createdat TIMESTAMP,
+	CONSTRAINT fk_cartID FOREIGN KEY(cartID) REFERENCES carts,
+	CONSTRAINT fk_productID FOREIGN KEY(productID) REFERENCES products
+	)`
+
+	_, err := p.db.Exec(query)
+	if err != nil {
+		return fmt.Errorf("failed to crate table cart item: %v", err)
+	}
+	return nil
+}
+
+func (p *Postgres) Close() error {
+	if p.db != nil {
+		return p.db.Close()
+	}
+	return nil
+}
+
+func (p *Postgres) Exec(query string, args ...interface{}) (sql.Result, error) {
+	return p.db.Exec(query, args...)
+}
+
+func (p *Postgres) QueryRow(query string, args ...interface{}) *sql.Row {
+	return p.db.QueryRow(query, args...)
 }
