f39d327d2d26b9978d38bdfe9e7c91beb5213f0d
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-06-22 16:25:09 +0300

    cart get/add handlers

--- cart-service/internal/handler/cart.go
+++ cart-service/internal/handler/cart.go
@@ -2,18 +2,29 @@ package handler
 
 import (
 	"log"
+
+	service "github.com/wycliff-ochieng/cart-service/internal/services"
+
 	"net/http"
 )
 
 type Cart struct {
 	l *log.Logger
+	k *service.CartService
 }
 
-func NewCart(l *log.Logger) *Cart {
-	return &Cart{l}
+func NewCartHandler(l *log.Logger, k *service.CartService) *Cart {
+	return &Cart{
+		l: l,
+		k: k,
+	}
 }
 
 func (c *Cart) CreateCart(w http.ResponseWriter, r *http.Request) error {
 	//c.store.CreateCart()
 	return nil
+}
+
+func (c *Cart) GetCart(w http.ResponseWriter, r *http.Request) {
+	return
 }
